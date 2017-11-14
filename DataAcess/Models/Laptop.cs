cac7c692c7e84c13d89d7dfcf50c74628c7f3b2e
phoon <geostamatov96@gmail.com>
2017-11-14 13:22:13 +0200

    Models commit

---
+++ DataAcess/Models/Laptop.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models.Interfaces;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Models
+{
+    public class Laptop : IEntity
+    {
+        [Column("LaptopID")]
+        public int Id { get; set; }
+
+        public string Brand { get; set; }
+
+        public string Model { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string OS { get; set; }
+        [Column(TypeName = "varbinary")]
+        public byte[] Image { get; set; }
+
+        public int Ram { get; set; }
+
+        public string Processor { get; set; }
+
+        public string HardDisk { get; set; }
+
+        public double Screen { get; set; }
+
+    }
+}
---
+++ DataAcess/Models/PC.cs
@@ -0,0 +1,28 @@
+﻿using Models.Interfaces;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Models
+{
+    public class PC : IEntity
+    {
+        [Column("PCID")]
+        public int Id { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string OS { get; set; }
+
+        public int Ram { get; set; }
+
+        public string Processor { get; set; }
+
+        public string HardDisk { get; set; }
+
+        public string VideoCard { get; set; }
+
+        public string Case { get; set; }
+
+        public string PowerSupply { get; set; }
+    }
+}
