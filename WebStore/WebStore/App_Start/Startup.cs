ff1ef7da2ff661f91a297284e960e7e0bdb8c8dc
Iliyan Stoyanov <virus_22@abv.bg>
2017-11-16 12:12:38 +0200

    Implemented Identity to MVC layer.

---
+++ WebStore/WebStore/App_Start/Startup.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebStore.Models;
+
+namespace WebStore.App_Start
+{
+    public class Startup
+    {
+        public static Func<UserManager<User>> UserManagerFactory { get; private set; }
+
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/auth/login")
+            });
+
+            // configure the user manager
+            UserManagerFactory = () =>
+            {
+                var usermanager = new UserManager<User>(
+                    new UserStore<User>(new WebStoreDbContext()));
+                // allow alphanumeric characters in username
+                usermanager.UserValidator = new UserValidator<User>(usermanager)
+                {
+                    AllowOnlyAlphanumericUserNames = false
+                };
+
+                return usermanager;
+            };
+
+        }
+    }
+}
---
+++ WebStore/WebStore/Models/WebStoreDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebStore.Models
+{
+    public class WebStoreDbContext : IdentityDbContext<User>
+    {
+        public WebStoreDbContext()
+            : base("DefaultConnection")
+        {
+#if DEBUG
+            Database.Log = s => System.Diagnostics.Debug.WriteLine(s);
+#endif
+
+            Configuration.LazyLoadingEnabled = false;
+            Configuration.ProxyCreationEnabled = false;
+        }
+    }
+}
